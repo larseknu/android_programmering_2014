26739256a8213b321460dd638cf738d6a8366d5e
Lars Emil Knudsen <larsemil87@gmail.com>
2014-02-12 21:16:51 +0100

    Lecture 04 Example 01 - Really Done

--- Lecture 04/PlayingWithActionBar/src/com/capgemini/playingwithactionbar/MainActivity.java
+++ Lecture 04/PlayingWithActionBar/src/com/capgemini/playingwithactionbar/MainActivity.java
@@ -2,6 +2,8 @@ package com.capgemini.playingwithactionbar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -11,6 +13,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		
+		return true;
 	}
 	
 	public void onOption1Clicked(MenuItem menuItem) {
