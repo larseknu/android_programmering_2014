dd201c52fb9cc47c63357eba83e9883bc5884e68
Lars Emil Knudsen <larsemil87@gmail.com>
2014-02-12 21:33:50 +0100

    Lecture 04 Example 03 - Done

--- Lecture 04/PlayingWithActionBar/src/com/capgemini/playingwithactionbar/MainActivity.java
+++ Lecture 04/PlayingWithActionBar/src/com/capgemini/playingwithactionbar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.capgemini.playingwithactionbar;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -24,18 +25,37 @@ public class MainActivity extends Activity {
 	}
 	
 	public void onOption1Clicked(MenuItem menuItem) {
+		setActionBarLogo();
 		Toast.makeText(this, "Option 1", Toast.LENGTH_LONG).show();
 	}
 	
 	public void onOption2Clicked(MenuItem menuItem) {
+		setActionBarTitleAndSubtitle();
 		Toast.makeText(this, "Option 2", Toast.LENGTH_LONG).show();
 	}
 	
 	public void onOption3Clicked(MenuItem menuItem) {
+		hideTitleAndSubtitle();
 		Toast.makeText(this, "Option 3", Toast.LENGTH_LONG).show();
 	}
 	
 	public void onExitClicked(MenuItem menuItem) {
 		finish();
+	}
+	
+	public void setActionBarLogo() {
+		ActionBar actionBar = getActionBar();
+		actionBar.setLogo(R.drawable.capgemini_logo);
+	}
+	
+	public void setActionBarTitleAndSubtitle() {
+		ActionBar actionBar = getActionBar();
+		actionBar.setTitle("Capgemini");
+		actionBar.setSubtitle("People Matter. Results Count.");
+	}
+	
+	public void hideTitleAndSubtitle() {
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowTitleEnabled(false);
 	}
 }
