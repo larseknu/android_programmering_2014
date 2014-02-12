f2d581e8cf944821223e2cc3b9b25d6ba8a41933
Lars Emil Knudsen <larsemil87@gmail.com>
2014-02-12 21:07:40 +0100

    Lesson 04 Example01 Done

--- Lecture 04/PlayingWithActionBar/src/com/capgemini/playingwithactionbar/MainActivity.java
+++ Lecture 04/PlayingWithActionBar/src/com/capgemini/playingwithactionbar/MainActivity.java
@@ -1,7 +1,9 @@
 package com.capgemini.playingwithactionbar;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	public void onOption1Clicked(MenuItem menuItem) {
+		Toast.makeText(this, "Option 1", Toast.LENGTH_LONG).show();
+	}
+	
+	public void onOption2Clicked(MenuItem menuItem) {
+		Toast.makeText(this, "Option 2", Toast.LENGTH_LONG).show();
+	}
+	
+	public void onOption3Clicked(MenuItem menuItem) {
+		Toast.makeText(this, "Option 3", Toast.LENGTH_LONG).show();
+	}
+	
+	public void onExitClicked(MenuItem menuItem) {
+		finish();
 	}
 }
