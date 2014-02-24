e2a0fcd84a3069037abe9df9561bee78226a917b
Lars Emil Knudsen <larsemil87@gmail.com>
2014-02-24 20:53:02 +0100

    Lecture 05 - Example 07.1 - Training#1

--- Lecture 05/PlayingWithLayouts/src/com/larseknu/playingwithlayouts/MainActivity.java
+++ Lecture 05/PlayingWithLayouts/src/com/larseknu/playingwithlayouts/MainActivity.java
@@ -2,31 +2,15 @@ package com.larseknu.playingwithlayouts;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewStub;
 
 public class MainActivity extends Activity {
-	private View smileys; 
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		ViewServer.get(this).addWindow(this);
-	}
-	
-	public void toggleSmileys(View view) {
-		if (smileys == null)
-		{
-			smileys = ((ViewStub) findViewById(R.id.happySmileys)).inflate();
-		}
-		else
-		{
-			boolean visible = (smileys.getVisibility() == View.VISIBLE);
-			smileys.setVisibility(visible ? View.GONE : View.VISIBLE);
-		}
 	}
 	
 	@Override
