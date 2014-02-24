cb9dba58edee51a4df8b9f907d084e497f9be8b5
Lars Emil Knudsen <larsemil87@gmail.com>
2014-02-24 20:37:10 +0100

    Lecture 05 - Example 06.3 - ViewStub

--- Lecture 05/PlayingWithLayouts/src/com/larseknu/playingwithlayouts/MainActivity.java
+++ Lecture 05/PlayingWithLayouts/src/com/larseknu/playingwithlayouts/MainActivity.java
@@ -1,18 +1,26 @@
 package com.larseknu.playingwithlayouts;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends Activity {
-
+	private View smileys; 
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		ViewServer.get(this).addWindow(this);
+		
+		smileys = findViewById(R.id.happySmileys);
+	}
+	
+	public void toggleSmileys(View view) {
+		boolean visible = (smileys.getVisibility() == View.VISIBLE);
+		smileys.setVisibility(visible ? View.GONE : View.VISIBLE);
 	}
 	
 	@Override
