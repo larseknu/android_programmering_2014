721588d4535d350550778d4fa54222ddc6d7212a
Lars Emil Knudsen <larsemil87@gmail.com>
2014-03-03 21:08:54 +0100

    Lecture 06 - Example 03.2 - Service Threading Considerations - Done

--- Lecture 06/PlayingWithServices/src/com/capgemini/playingwithservices/WorkerService.java
+++ Lecture 06/PlayingWithServices/src/com/capgemini/playingwithservices/WorkerService.java
@@ -1,5 +1,8 @@
 package com.capgemini.playingwithservices;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
@@ -7,20 +10,26 @@ import android.os.IBinder;
 
 public class WorkerService extends Service {
 	Worker _worker;
+	ExecutorService _executorService;
 
 	@Override
 	public void onCreate() {
 		_worker = new Worker(this);
 		_worker.MonitorGpsInBackground();
+		_executorService = Executors.newSingleThreadExecutor();
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		Location location = _worker.getLocation();
-		
-		String address = _worker.reverseGeocode(location);
-		
-		_worker.save(location, address, "WorkerService.out");
+		_executorService.execute(new Runnable() {
+			public void run() {
+				Location location = _worker.getLocation();
+				
+				String address = _worker.reverseGeocode(location);
+				
+				_worker.save(location, address, "WorkerService.out");
+			}
+		});
 		
 		return Service.START_NOT_STICKY;
 	}
