582977fc115685f155730d1df2784cbb58a36318
Lars Emil Knudsen <larsemil87@gmail.com>
2014-03-03 21:21:06 +0100

    Lecture 06 - Example 03.3 - Taking control of service lifetime - Done

--- Lecture 06/PlayingWithServices/src/com/capgemini/playingwithservices/WorkerService.java
+++ Lecture 06/PlayingWithServices/src/com/capgemini/playingwithservices/WorkerService.java
@@ -2,6 +2,8 @@ package com.capgemini.playingwithservices;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import android.app.Service;
 import android.content.Intent;
@@ -11,25 +13,20 @@ import android.os.IBinder;
 public class WorkerService extends Service {
 	Worker _worker;
 	ExecutorService _executorService;
+	ScheduledExecutorService _scheduledStopService;
 
 	@Override
 	public void onCreate() {
 		_worker = new Worker(this);
 		_worker.MonitorGpsInBackground();
 		_executorService = Executors.newSingleThreadExecutor();
+		_scheduledStopService = Executors.newSingleThreadScheduledExecutor();
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		_executorService.execute(new Runnable() {
-			public void run() {
-				Location location = _worker.getLocation();
-				
-				String address = _worker.reverseGeocode(location); 
-				
-				_worker.save(location, address, "WorkerService.out");
-			}
-		});
+		ServiceRunnable runnable = new ServiceRunnable(this, startId);
+		_executorService.execute(runnable);
 		
 		return Service.START_NOT_STICKY;
 	}
@@ -42,5 +39,41 @@ public class WorkerService extends Service {
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
+	}
+	
+	class ServiceRunnable implements Runnable {
+		WorkerService _theService;
+		int _startId;
+		
+		public ServiceRunnable(WorkerService theService, int startId) {
+			_theService = theService;
+			_startId = startId;
+		}
+		
+		public void run() {
+			LogHelper.ProcessAndThreadId("WorkerService.onStartCommand");
+			Location location = _worker.getLocation();
+	
+			String address = _worker.reverseGeocode(location);
+			
+			_worker.save(location, address, "WorkerService.out");
+			
+			DelayedStopRequest delayedStopRequest = new DelayedStopRequest(_theService, _startId);
+			_theService._scheduledStopService.schedule(delayedStopRequest, 10, TimeUnit.SECONDS);
+		}
+	}
+	
+	class DelayedStopRequest implements Runnable {
+		WorkerService _theService;
+		int _startId;
+		
+		public DelayedStopRequest(WorkerService theService, int startId) {
+			_theService = theService;
+			_startId = startId;
+		}
+		
+		public void run() {
+			_theService.stopSelfResult(_startId);
+		}
 	}
 }
