dce32b651486a8b2fd36f09e28279044d06fda3e
Lars Emil Knudsen <larsemil87@gmail.com>
2014-03-03 19:47:16 +0100

    Lecture 06 - Example 01.3 - Threads - Done

--- Lecture 06/PlayingWithThreads/src/com/capgemini/playingwiththreads/MainActivity.java
+++ Lecture 06/PlayingWithThreads/src/com/capgemini/playingwiththreads/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	TextView _outputTextView;
+	Thread _workerThread;
+	String _tempMessage;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +23,34 @@ public class MainActivity extends Activity {
 	}
 	
 	public void doWork() {
-		Worker worker = new Worker(this);
-		_outputTextView.setText("Starting");
-		
-		Location location = worker.getLocation();
-		_outputTextView.setText("Retrieved Location");
-		
-		String address = worker.reverseGeocode(location);
-		_outputTextView.setText("Retrieved Address");
-		
-		worker.save(location, address, "FancyFileName.out");
-		_outputTextView.setText("Done");
+		_workerThread = new Thread(new Runnable() {
+			public void run() {
+				Worker worker = new Worker(MainActivity.this);
+				updateUI("Starting");
+				
+				Location location = worker.getLocation();
+				updateUI("Retrieved Location");
+				
+				String address = worker.reverseGeocode(location);
+				updateUI("Retrieved Address");
+				
+				worker.save(location, address, "FancyFileName.out");
+				updateUI("Done");
+			}
+		});
+		_workerThread.start();
 	}
 
+	public void updateUI(String message) {
+		_tempMessage = message;
+		
+		_outputTextView.post(new Runnable() {
+			public void run() {
+				_outputTextView.setText(_tempMessage);
+			}
+		});
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
