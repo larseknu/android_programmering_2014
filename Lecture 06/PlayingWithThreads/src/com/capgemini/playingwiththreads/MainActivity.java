7f63e163ebe7269514c681c36e3d17ce10f8c246
Lars Emil Knudsen <larsemil87@gmail.com>
2014-03-03 14:57:06 +0100

    Lecture 06 - Example 01.3 - Threads

--- Lecture 06/PlayingWithThreads/src/com/capgemini/playingwiththreads/MainActivity.java
+++ Lecture 06/PlayingWithThreads/src/com/capgemini/playingwiththreads/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		_outputTextView = (TextView) findViewById(R.id.outputTextView); 
-		StrictMode.enableDefaults();
+		StrictMode.enableDefaults(); 
 	}
 	
 	public void doWork() {
