cfef826b8f9df9b3281ee0ade917a2dddfbeee67
Lars Emil Knudsen <larsemil87@gmail.com>
2014-03-17 15:03:38 +0100

    Lecture 08 - Example 01 - Done

--- Lecture 08/src/com/capgemini/playingwithsqlite/database/SQLiteHelper.java
+++ Lecture 08/src/com/capgemini/playingwithsqlite/database/SQLiteHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class SQLiteHelper extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "show.db";
-	private static final int DATABASE_VERSION = 3;
+	private static final int DATABASE_VERSION = 1;
 	
 	public SQLiteHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
