96ba2d923f3732b21f46c7f23cba106881424a04
Lars Emil Knudsen <larsemil87@gmail.com>
2014-03-30 19:42:16 +0200

    Lecture 09 - Example 02 - Markers and Camera - Done

--- Lecture 09/PlayingWithGoogleMaps/src/com/capgemini/playingwithgooglemaps/MainActivity.java
+++ Lecture 09/PlayingWithGoogleMaps/src/com/capgemini/playingwithgooglemaps/MainActivity.java
@@ -1,18 +1,38 @@
 package com.capgemini.playingwithgooglemaps;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MainActivity extends ActionBarActivity {
+	private GoogleMap map;
+	private LatLng HIOF = new LatLng(59.12797849, 11.35272861);
+	private LatLng FREDRIKSTAD = new LatLng(59.21047628, 10.93994737);
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+		map = mapFragment.getMap();
+		
+		map.addMarker(new MarkerOptions().position(HIOF).title("Østfold University College"));
+		map.addMarker(new MarkerOptions().position(FREDRIKSTAD).title("Fredrikstad Kino"));
+		
+		map.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(HIOF, 13, 0, 0)));
+		
+		map.animateCamera(CameraUpdateFactory.newLatLng(FREDRIKSTAD), 2000, null);
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
